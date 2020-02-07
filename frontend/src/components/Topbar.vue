0bd19a0045eace512e7a89690b76ffdefe6d83b2
Jaffer <2194838N@student.gla.ac.uk>
2020-02-07 20:55:32 +0000

    Implement backend for search bar and add more favicons.

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -6,6 +6,30 @@
     <button v-else id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
       <i class="fa fa-bars"></i>
     </button>
+    <div v-if="!nosidebar">
+      <div class="ml-md-2 my-2 my-md-0 navbar-search input-group">
+        <input v-model="query" type="text" class="form-control bg-light small" placeholder="Search for an incident..." aria-label="Search for an incident">
+        <div class="input-group-append">
+          <button class="btn btn-primary" type="button">
+            <i class="fas fa-search fa-sm"></i>
+          </button>
+        </div>
+      </div>
+      <div v-if="query.length" class="card-body" style="position:absolute;top:55px;left:20px;background-color:white;z-index:99;overflow-y:auto;max-height:20em">
+        <ul v-for="result in queryResults" :key="result.id">
+          <li :class="['list-group-item', 'list-group-flush', 'task', result.open ? '' : 'text-muted']">
+            <div class="row align-items-center no-gutters">
+                <div class="col mr-2">
+                    <h6 class="mb-0">
+                        <strong>{{ result.name }} </strong>
+                    </h6>
+                </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      {{ queryResults.length }}
+    </div>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown no-arrow mx-1">
         <a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -37,12 +61,20 @@
 </template>
 
 <script>
+import fz from 'fuzzaldrin-plus'
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'Topbar',
   props: {
     nosidebar: {
       type: Boolean,
       default: false
+    }
+  },
+  data () {
+    return {
+      query: ''
     }
   },
   methods: {
@@ -56,7 +88,31 @@ export default {
     },
     avatarUrl: function () {
       return this.$store.getters['user/getAvatarUrl']
-    }
+    },
+    queryResults () {
+      if (!this.query) return this.allIncidents
+
+      const preparedQuery = fz.prepareQuery(this.query)
+      const scores = {}
+
+      return this.allIncidents
+        .map((incident, index) => {
+          const scorableFields = [
+            incident.name,
+            incident.description,
+            incident.location.properties.address
+          ].map(toScore => fz.score(toScore, this.query, { preparedQuery }))
+
+          scores[incident.id] = Math.max(...scorableFields)
+
+          return incident
+        })
+        .filter(option => scores[option.id] > 1)
+        .sort((a, b) => scores[b.id] - scores[a.id])
+    },
+    ...mapGetters({
+      allIncidents: 'incidents/getIncidents'
+    })
   }
 }
 </script>
